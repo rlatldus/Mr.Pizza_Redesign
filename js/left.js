faf975e5096ed16ef64b3f61566c10aa156a2918
ssing_7942 <djagmlwnn12@gmail.com>
2023-05-23 14:01:43 +0900

    zxxx

--- js/left.js
+++
@@ -1,5 +0,0 @@
-
-const fullHeightDiv = document.getElementById('left');
-const documentHeight = document.documentElement.scrollHeight; // 문서 전체의 높이
-console.log(documentHeight);
-fullHeightDiv.style.height = documentHeight + 'px';
