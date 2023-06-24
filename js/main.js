545acec20332ebabc6617ed898b914fb61f439fa
ssing_7942 <djagmlwnn12@gmail.com>
2023-06-25 03:06:31 +0900

    ?

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
 // 공지사항
 let pages = 0;//현재 인덱스 번호
 let positionValue = 0;//images 위치값
-const IMAGE_WIDTH = 280;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
+const IMAGE_WIDTH = 8.1711;//한번 이동 시 IMAGE_WIDTH만큼 이동한다.
 //DOM
 const backBtn = document.querySelector(".nt_prev")
 const nextBtn = document.querySelector(".nt_next")
@@ -54,7 +54,7 @@ function next() {
 	if (pages < 3) {
 		backBtn.removeAttribute('disabled')//뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
 		positionValue -= IMAGE_WIDTH;//IMAGE_WIDTH의 증감을 positionValue에 저장한다.
-		images.style.transform = `translateX(${positionValue}px)`;
+		images.style.transform = `translateX(${positionValue}%)`;
 		//x축으로 positionValue만큼의 px을 이동한다.
 		pages += 1; //다음 페이지로 이동해서 pages를 1증가 시킨다.
 	}
@@ -67,7 +67,7 @@ function back() {
 	if (pages > 0) {
 		nextBtn.removeAttribute('disabled')
 		positionValue += IMAGE_WIDTH;
-		images.style.transform = `translateX(${positionValue}px)`;
+		images.style.transform = `translateX(${positionValue}%)`;
 		pages -= 1; //이전 페이지로 이동해서 pages를 1감소 시킨다.
 	}
 	if (pages === 0) {
